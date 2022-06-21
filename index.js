c2ade65f84152b94666c96eee193ddd83e1b5329
hvisor <kaweewat.k@gmail.com>
2022-06-21 15:44:29 +0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -29,27 +29,33 @@ async function send(e) {
   e.preventDefault();
   const prsno = document.getElementById('prsno').value;
   const pwd = document.getElementById('pwd').value;
+
+  fireSweetAlert();
   // 3. axios post data
-  try {
-    const result = await axios.post(
-      'https://68fc-110-49-90-2.ngrok.io/v1/register',
-      {
-        prsno: prsno,
-        pwd: pwd,
-        fullname: '',
-        userId: userId,
-        pictureUrl: pictureUrl,
-        displayName: displayName,
-        liffId: liffId,
-      }
-    );
-    if (result.status == 200) {
-      // console.log(result)
-      liff.closeWindow();
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //     const result = await axios.post(
+  //         "https://68fc-110-49-90-2.ngrok.io/v1/register", {
+  //             prsno: prsno,
+  //             pwd: pwd,
+  //             fullname: '',
+  //             userId: userId,
+  //             pictureUrl: pictureUrl,
+  //             displayName: displayName,
+  //             liffId: liffId,
+  //         }
+  //     )
+  //     if (result.status == 200) {
+  //         liff.closeWindow();
+  //     }else{
+  //         fireSweetAlert()
+  //     }
+  // } catch (error) {
+  //     console.log(error)
+  // }
+}
+
+function fireSweetAlert() {
+  Swal.fire('Good job!', 'You clicked the button!', 'success');
 }
 
 // https://tall-trains-search-49-237-22-19.loca.lt/v1/register
